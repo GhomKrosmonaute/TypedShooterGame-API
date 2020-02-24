a53fc5f9db4de4994fc6588f303e01b26d635321
Camille Abella <camille.abella@hotmail.fr>
2020-02-24 16:40:14 +0100

    format router.js and remove .gitkeep files

--- api/router.js
+++ api/router.js
@@ -3,21 +3,17 @@ const express = require('express')
 const controllers = require('./controllers')
 const routes = require('./routes.json')
 
-module.exports = (() => {
+const router = express.Router()
 
-    const router = express.Router()
+for(const route in routes){
+    const [ fullmatch, method, path ] = /^([A-Z]+)(.+)$/.exec(route)
+    const controllerName = routes[route]
+    router.route(path)[method.toLowerCase()](( req, res ) => {
+        return controllers[controllerName]( req, res )
+    })
+    console.log('Route loaded:', method, path)
+}
 
-    for(const route in routes){
-        const [ fullmatch, method, path ] = /^([A-Z]+)(.+)$/.exec(route)
-        const controllerName = routes[route]
-        router.route(path)[method.toLowerCase()](( req, res ) => {
-            return controllers[controllerName]( req, res )
-        })
-        console.log('Route loaded:', method, path)
-    }
-    
-    console.log('Router ready:', router.stack.length, 'routes')
-    
-    return router
+console.log('Router ready:', router.stack.length, 'routes')
 
-})()
+module.exports = router
