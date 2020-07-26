b2de6fcfde38217f772cc5b81f609084ccf044be
CamilleAbella <camille.abella@hotmail.fr>
2020-07-26 13:14:35 +0000

    backup

--- api/controllers/leaderBoard.js
+++ api/controllers/leaderBoard.js
@@ -8,116 +8,62 @@ async function getLeaderBoard(req, res) {
 
     switch (mode) {
         case 0:
-            leaderBoard = {
-                top: await this.exec(`
-                    SELECT
-                        pl.username, pr.score, pr.\`precision\`, pr.kills, pr.duration
-                    FROM party pr
-                    LEFT JOIN player pl 
-                    ON pr.player_id = pl.id
-                    WHERE pr.score > 0
-                    ORDER BY 
-                        pr.score DESC,
-                        pr.\`precision\` DESC,
-                        pr.duration ASC,
-                        pr.kills DESC
-                    LIMIT 30
-                `),
-                player: (await this.exec(`
-                    SELECT
-                        pl.username, pr.score, pr.\`precision\`, pr.kills, pr.duration
-                    FROM party pr
-                    LEFT JOIN player pl
-                    ON pr.player_id = pl.id
-                    WHERE pr.player_id = ?
-                    ORDER BY 
-                        pr.score DESC,
-                        pr.\`precision\` DESC,
-                        pr.duration ASC,
-                        pr.kills DESC
-                    LIMIT 1
-                `,[id]))[0]
-            }
+            leaderBoard = await this.exec(`
+                SELECT
+                    pl.username, pr.score, pr.\`precision\`, pr.kills, pr.duration
+                FROM party pr
+                LEFT JOIN player pl 
+                ON pr.player_id = pl.id
+                WHERE pr.score > 0
+                ORDER BY 
+                    pr.score DESC,
+                    pr.\`precision\` DESC,
+                    pr.duration ASC,
+                    pr.kills DESC
+                LIMIT 30
+            `)
             break
         case 1:
-            leaderBoard = {
-                top: await this.exec(`
-                    SELECT
-                        pl.username, 
-                        AVG(pr.score) AS score, 
-                        AVG(pr.\`precision\`) AS \`precision\`,
-                        AVG(pr.kills) AS kills, 
-                        AVG(pr.duration) AS duration
-                    FROM player pl
-                    LEFT JOIN party pr
-                    ON pr.player_id = pl.id
-                    WHERE pr.score > 0
-                    ORDER BY
-                        score DESC,
-                        \`precision\` DESC,
-                        duration ASC,
-                        kills DESC
-                    LIMIT 30
-                `),
-                player: (await this.exec(`
-                    SELECT
-                        pl.username, 
-                        AVG(pr.score) AS score, 
-                        AVG(pr.\`precision\`) AS \`precision\`,
-                        AVG(pr.kills) AS kills, 
-                        AVG(pr.duration) AS duration
-                    FROM player pl
-                    LEFT JOIN party pr
-                    ON pr.player_id = pl.id
-                    WHERE pl.id = ?
-                    ORDER BY
-                        score DESC,
-                        \`precision\` DESC,
-                        duration ASC,
-                        kills DESC
-                    LIMIT 1
-                `,[id]))[0]
-            }
+            leaderBoard = await this.exec(`
+                SELECT
+                    pl.username,
+                    AVG(pr.score) AS score,
+                    AVG(pr.\`precision\`) AS \`precision\`,
+                    AVG(pr.kills) AS kills,
+                    AVG(pr.duration) AS duration
+                FROM player pl
+                LEFT JOIN party pr
+                ON pr.player_id = pl.id
+                WHERE score IS NOT NULL
+                GROUP BY pl.username
+                ORDER BY
+                    score DESC,
+                    \`precision\` DESC,
+                    duration ASC,
+                    kills DESC
+                LIMIT 30
+            `)
             break
         case 2:
-            leaderBoard = {
-                top: await this.exec(`
-                    SELECT
-                        pl.username, 
-                        SUM(pr.score) AS score, 
-                        AVG(pr.\`precision\`) AS \`precision\`,
-                        SUM(pr.kills) AS kills, 
-                        SUM(pr.duration) AS duration
-                    FROM player pl
-                    LEFT JOIN party pr
-                    ON pr.player_id = pl.id
-                    WHERE pr.score > 0
-                    ORDER BY
-                        score DESC,
-                        \`precision\` DESC,
-                        duration ASC,
-                        kills DESC
-                    LIMIT 30
-                `),
-                player: (await this.exec(`
-                    SELECT
-                        pl.username, 
-                        SUM(pr.score) AS score, 
-                        AVG(pr.\`precision\`) AS \`precision\`,
-                        SUM(pr.kills) AS kills, 
-                        SUM(pr.duration) AS duration
-                    FROM player pl
-                    LEFT JOIN party pr
-                    ON pr.player_id = pl.id
-                    WHERE pl.id = ?
-                    ORDER BY
-                        score DESC,
-                        \`precision\` DESC,
-                        duration ASC,
-                        kills DESC
-                    LIMIT 1
-                `,[id]))[0]
-            }
+            leaderBoard = await this.exec(`
+                SELECT
+                    pl.username,
+                    SUM(pr.score) AS score,
+                    AVG(pr.\`precision\`) AS \`precision\`,
+                    SUM(pr.kills) AS kills,
+                    SUM(pr.duration) AS duration
+                FROM player pl
+                LEFT JOIN party pr
+                ON pr.player_id = pl.id
+                WHERE score IS NOT NULL
+                GROUP BY pl.username
+                ORDER BY
+                    score DESC,
+                    \`precision\` DESC,
+                    duration ASC,
+                    kills DESC
+                LIMIT 30
+            `)
             break
     }
 
